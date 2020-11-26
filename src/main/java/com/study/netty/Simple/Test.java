b7e25f48f8d171630ea0b0362e14adf4b00f3d25
jianghui <1242928326@qq.com>
2020-11-26 15:15:36 +0800

    删除分支文件

--- src/main/java/com/study/netty/Simple/Test.java
+++
@@ -1,9 +0,0 @@
-package com.study.netty.Simple;
-
-import io.netty.util.NettyRuntime;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println(NettyRuntime.availableProcessors());
-    }
-}
--- src/main/java/com/study/netty/dubborpc/publicinterface/HelloService.java
+++
@@ -1,8 +0,0 @@
-package com.study.netty.dubborpc.publicinterface;
-
-/**
- * 这是个接口服务方和消费方共有的
- */
-public interface HelloService {
-    String hello(String msg);
-}
